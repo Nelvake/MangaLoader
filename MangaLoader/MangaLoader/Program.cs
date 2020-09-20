dc761d6b42e54196129ad4372573735111541b95
NickSagalbaev <NickSagalbaev@gmail.com>
2020-09-20 19:35:15 +0600

    Init commit

---
+++ MangaLoader/MangaLoader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MangaLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
