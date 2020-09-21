cc4860c12da33d685daddfacd633e9f255649bba
NickSagalbaev <NickSagalbaev@gmail.com>
2020-09-21 17:21:30 +0600

    feat <Chapter,ChapterInfo>: Созданы сущности Главы и информации о ней.

---
+++ MangaLoader/ML.Domain/Chapter.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.Domain
+{
+    public class Chapter
+    {
+        [JsonProperty("chapter")]
+        public ChapterInfo ChapterInfo { get; set; }
+        [JsonProperty("images")]
+        public List<string> Images { get; set; } = new List<string>();
+    }
+}
---
+++ MangaLoader/ML.Domain/ChapterInfo.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.Domain
+{
+    public class ChapterInfo
+    {
+        [JsonProperty("volume")]
+        public int Volume { get; set; }
+        [JsonProperty("number")]
+        public string Number { get; set; }
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+    }
+}
