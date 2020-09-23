24b4033d4d6d0a3a5d793b852a02592a8a9baa17
NickSagalbaev <NickSagalbaev@gmail.com>
2020-09-23 22:01:28 +0600

    feat <Console.Program> : Добавлена CLI обработка аргументов.

---
+++ MangaLoader/ML.Service/Options/ConsoleOption.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.Service.Options
+{
+    public class ConsoleOption
+    {
+        [Option('l', "login", HelpText = "Login for authorization", Required = true)]
+        public string Login { get; set; }
+
+        [Option('p', "password", HelpText = "Password for authorization", Required = true)]
+        public string Password { get; set; }
+
+        [Option('u', "url", HelpText = "Url manga", Required = true)]
+        public string MangaLink { get; set; }
+    }
+}
--- MangaLoader/MangaLoader/Program.cs
+++ MangaLoader/MangaLoader/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using ML.Service;
+using ML.Service.Interfaces;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using CommandLine;
+using System.Linq;
+using ML.Service.Options;
 
 namespace MangaLoader
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                await Parser.Default.ParseArguments<ConsoleOption>(args).WithParsedAsync(async x =>
+                {
+                    var generalUrl = "https://mangalib.me/";
+
+                    var stopwatch = new Stopwatch();
+                    IAuth authService = new AuthService(generalUrl);
+                    ILib chapterService = new ChapterService(generalUrl, await authService.AuthAsync(x.Login, x.Password));
+
+                    Console.WriteLine("Getting information about a manga...");
+
+                    var chaptersId = await chapterService.GetChaptersId(x.MangaLink);
+                    var chapters = await chapterService.GetChaptersInfo(chaptersId);
+
+                    Console.WriteLine("Starting download...");
+
+                    stopwatch.Start();
+                    chapterService.DownloadChapters(chapters);
+                    stopwatch.Stop();
+
+                    Console.WriteLine($"Download completed, elapsed time - {stopwatch.Elapsed}");
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
         }
     }
 }
