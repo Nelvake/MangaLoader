0e596f302297845d24dd4760b9286507ab72807b
NickSagalbaev <NickSagalbaev@gmail.com>
2020-09-23 16:32:35 +0600

    feat <Service.Interfaces> : Добавил интерфейсы для возможности дальнейшнго расширения.

---
+++ MangaLoader/ML.Service.Interfaces/IAuth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML.Service.Interfaces
+{
+    public interface IAuth
+    {
+        Task<List<string>> AuthAsync(string email, string password);
+    }
+}
---
+++ MangaLoader/ML.Service.Interfaces/ILib.cs
@@ -0,0 +1,17 @@
+﻿using ML.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML.Service.Interfaces
+{
+    public interface ILib
+    {
+        Task<Chapter> GetChapterInfo(string chapterId);
+        Task<List<Chapter>> GetChaptersInfo(List<string> chaptersId);
+        bool DownloadChapter(Chapter chapter);
+        bool DownloadChapters(List<Chapter> chapters);
+        Task<List<string>> GetChaptersId(string url);
+    }
+}
